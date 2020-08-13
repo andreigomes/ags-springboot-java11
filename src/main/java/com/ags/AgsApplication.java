a627d8fd1c79d23f722a6683d4a32b59b3a90c6a
andreigomes <andrei_sps@hotmail.com>
2020-08-13 01:12:57 -0300

    Inicio do  projeto

---
+++ src/main/java/com/ags/AgsApplication.java
@@ -0,0 +1,13 @@
+package com.ags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ags/AgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
