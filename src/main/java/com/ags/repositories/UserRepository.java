407bf07f50a95a18d2c26ac29b9679ffd4bce24f
andreigomes <andrei_sps@hotmail.com>
2020-08-17 00:09:42 -0300

    Service layer. component registration

--- src/main/java/com/ags/repositories/UserRepository.java
+++ src/main/java/com/ags/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.ags.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ags.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
